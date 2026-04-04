358caef1e193a23b6dc79439c0771e0d8c069ab0
yocloud-code <yocloud-code@users.noreply.github.com>
2026-04-04 16:45:36 +0000

    feat: add new MiniMax models (M2.7/M2.5/speech-2.8/2.6/image-01)
    - Add chat models: M2.7, M2.7-highspeed, M2.5, M2.5-highspeed, M2-her
    - Add TTS models: speech-2.8-hd/turbo, speech-2.6-hd/turbo
    - Add image model: image-01
    - Rename M2.1-lightning to M2.1-highspeed (upstream sync)
    - Add image generation URL routing (/v1/image_generation)

--- relay/channel/minimax/constants.go
+++ relay/channel/minimax/constants.go
@@ -3,20 +3,33 @@ package minimax
 // https://www.minimaxi.com/document/guides/chat-model/V2?id=65e0736ab2845de20908e2dd
 
 var ModelList = []string{
+	// Chat models
+	"MiniMax-M2.7",
+	"MiniMax-M2.7-highspeed",
+	"MiniMax-M2.5",
+	"MiniMax-M2.5-highspeed",
+	"M2-her",
+	"MiniMax-M2.1",
+	"MiniMax-M2.1-highspeed",
+	"MiniMax-M2",
 	"abab6.5-chat",
 	"abab6.5s-chat",
 	"abab6-chat",
 	"abab5.5-chat",
 	"abab5.5s-chat",
+	// TTS models
+	"speech-2.8-hd",
+	"speech-2.8-turbo",
+	"speech-2.6-hd",
+	"speech-2.6-turbo",
 	"speech-2.5-hd-preview",
 	"speech-2.5-turbo-preview",
 	"speech-02-hd",
 	"speech-02-turbo",
 	"speech-01-hd",
 	"speech-01-turbo",
-	"MiniMax-M2.1",
-	"MiniMax-M2.1-lightning",
-	"MiniMax-M2",
+	// Image models
+	"image-01",
 }
 
 var ChannelName = "minimax"
